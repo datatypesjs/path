7d46560117e1dad53e75016cbfa4f21e9bf93c88
Adrian Sieber <mail@adriansieber.com>
2016-11-29 09:58:05 +0000

    Run linter during tests, fix formatting

--- index.js
+++ index.js
@@ -1,107 +1,113 @@
-var path = require('path')
+const path = require('path')
+
+module.exports = (pathElement) => {
+  const betterPath = {}
+  let pathObject = {}
+
+  function parsePathString (pathString) {
+    const fileName = path.basename(pathString)
+    const extname = path.extname(pathString)
+    const isDotfile = fileName[0] === '.'
+    const fileNameParts = fileName.split('.')
+    const extensionParts = fileNameParts.slice(isDotfile ? 2 : 1)
+    const extension = extensionParts.length > 0
+      ? '.' + extensionParts.join('.')
+      : ''
 
 
-module.exports = function (pathElement) {
+    pathObject = {
+      path: path.normalize(pathString),
+      directoryPath: path.dirname(pathString),
+      extension: extension,
+      extensionParts: extensionParts.map(ext => {
+        // TODO: Reverse order
+        return '.' + ext
+      }),
+      baseName: path.basename(pathString, extension),
+      fileName: fileName,
+      fileType: extname.slice(1),
+      isDotfile: isDotfile,
+    }
 
-	var betterPath = {},
-		pathObject = {}
+    pathObject.format = pathObject.fileType
+    // TODO: Might break under windows
+    pathObject.isAbsolute = pathObject.path.slice(0, 1) === path.delimiter
 
-	function parsePathString (pathString) {
+    return pathObject
+  }
 
-		var fileName = path.basename(pathString),
-			extname = path.extname(pathString),
-			isDotfile = (fileName[0] === '.'),
-			fileNameParts = fileName.split('.'),
-			extensionParts = fileNameParts.slice(isDotfile ? 2 : 1),
-			extension = (extensionParts.length > 0) ?
-			            '.' + extensionParts.join('.') : ''
+  betterPath.path = function (pathString) {
 
+    if (pathString) {
+      pathObject = parsePathString(pathString)
+      return betterPath
+    }
+    else {
+      return pathObject.path
+    }
+  }
 
-		pathObject = {
-			path: path.normalize(pathString),
-			directoryPath: path.dirname(pathString),
-			extension: extension,
-			extensionParts: extensionParts.map(function (ext) {
-				// TODO: Reverse order
-				return '.' + ext
-			}),
-			baseName: path.basename(pathString, extension),
-			fileName: fileName,
-			fileType: extname.slice(1),
-			isDotfile: isDotfile
-		}
+  betterPath.baseName = function (pathString) {
 
-		pathObject.format = pathObject.fileType
-		// TODO: Might break under windows
-		pathObject.isAbsolute = (pathObject.path.slice(0, 1) === path.delimiter)
+    if (pathString) {
+      throw new Error('Does not yet support setting the baseName value.')
+    }
+    else {
+      return pathObject.baseName
+    }
+  }
 
-		return pathObject
-	}
+  betterPath.fileName = function (pathString) {
 
-	betterPath.path = function (pathString) {
+    if (pathString) {
+      throw new Error('Does not yet support setting the baseName value.')
+    }
+    else {
+      return pathObject.fileName
+    }
+  }
 
-		if (pathString) {
-			pathObject = parsePathString(pathString)
-			return betterPath
-		}
-		else
-			return pathObject.path
-	}
+  betterPath.toObject = function (localPathObject) {
 
-	betterPath.baseName = function (pathString) {
+    if (localPathObject) {
+      pathObject = localPathObject
+      return betterPath
+    }
+    else {
+      return pathObject
+    }
+  }
 
-		if (pathString) {
-			throw new Error('Does not yet support setting the baseName value.')
-		}
-		else
-			return pathObject.baseName
-	}
+  betterPath.fileType = function (type) {
+    if (type) {
+      if (pathObject.isDotfile) {
+        pathObject = parsePathString(pathObject.path + '.' + type)
+      }
+      else {
+        pathObject = parsePathString(
+          pathObject.path.replace(/[^.]*$/, type)
+        )
+      }
 
-	betterPath.fileName = function (pathString) {
+      return betterPath
+    }
+    else {
+      return pathObject.fileType
+    }
+  }
 
-		if (pathString) {
-			throw new Error('Does not yet support setting the baseName value.')
-		}
-		else
-			return pathObject.fileName
-	}
-
-	betterPath.toObject = function (localPathObject) {
-
-		if (localPathObject) {
-			pathObject = localPathObject
-			return betterPath
-		}
-		else
-			return pathObject
-	}
-
-	betterPath.fileType = function (type) {
-		if (type) {
-			if (pathObject.isDotfile)
-				pathObject = parsePathString(pathObject.path + '.' + type)
-			else
-				pathObject = parsePathString(
-					pathObject.path.replace(/[^.]*$/, type)
-				)
-
-			return betterPath
-		}
-		else
-			return pathObject.fileType
-	}
-
-	betterPath.format = betterPath.fileType
+  betterPath.format = betterPath.fileType
 
 
-	if (pathElement) {
-		if (typeof pathElement === 'string') {
-			betterPath.path(pathElement)
-			return betterPath
-		}
-		else
-			betterPath.pathObject(pathElement)
-	}
+  if (pathElement) {
+    if (typeof pathElement === 'string') {
+      betterPath.path(pathElement)
+      return betterPath
+    }
+    else {
+      betterPath.pathObject(pathElement)
+    }
+  }
 
-	return betterPath
+  return betterPath
 }
--- test/index.js
+++ test/index.js
@@ -1,38 +1,31 @@
-var assert = require('assert'),
-	betterPath = require('../index.js'),
-	filePath = '/path/to/a/file.txt',
-	directoryPath = '/path/to/some/files'
+const assert = require('assert')
+const betterPath = require('../index.js')
+const filePath = '/path/to/a/file.txt'
 
+{
+  console.info('returns new path-object') // eslint-disable-line no-console
+  const pathObject = betterPath(filePath)
 
-describe('Better Path', function () {
+  assert.equal(typeof pathObject, 'object')
 
-	it('returns new path-object', function () {
+  assert.equal(typeof pathObject.path, 'function')
+  assert.equal(typeof pathObject.fileName, 'function')
+  assert.equal(typeof pathObject.baseName, 'function')
+}
 
-		var pathObject = betterPath(filePath)
+{
+  console.info('has a path getter') // eslint-disable-line no-console
+  const pathObject = betterPath(filePath)
 
-		assert.equal(typeof pathObject, 'object')
+  assert.equal(pathObject.path(), filePath)
+}
 
-		assert.equal(typeof pathObject.path, 'function')
-		assert.equal(typeof pathObject.fileName, 'function')
-		assert.equal(typeof pathObject.baseName, 'function')
-	})
+{
+  console.info('has a path setter') // eslint-disable-line no-console
+  const pathObject = betterPath(filePath)
+  const newPath = '/path/to/another/file.txt'
 
+  pathObject.path(newPath)
 
-	it('has a path getter', function () {
-
-		var pathObject = betterPath(filePath)
-
-		assert.equal(pathObject.path(), filePath)
-	})
-
-
-	it('has a path setter', function () {
-
-		var pathObject = betterPath(filePath),
-			newPath = '/path/to/another/file.txt'
-
-		pathObject.path(newPath)
-
-		assert.equal(pathObject.path(), newPath)
-	})
-})
+  assert.equal(pathObject.path(), newPath)
+}
